f176f5d90e7f1bc9f172b83593377aca6831b7bf
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-02-20 13:13:49 +0600

    voting final prectic commit

--- assingment-4/prectic.js
+++ assingment-4/prectic.js
@@ -1,31 +1,29 @@
 
 function avrgVote(vote){
-    let mango = 0;
-    let banana = 0;
-   
-    for(let i=0; i<vote.length; i++){
-       let votts = vote[i]
-       if(votts == mango){
-           mango++;
-       }
-       else if(votts == banana){
-           banana++;
-       }
-    }
-    
-      if(mango>banana){
-         return "Mango";
-        }
-        else if(banana>mango){
-         return "Banana";
-        }
-        else{
+let mango = 0;
+let banana = 0;
 
-            return 'draw'
-        }
-   }
+for(let i=0; i<vote.length; i++){
+    let allVote = vote[i];
+    if(allVote == "mango"){
+        mango++;
+    }
+    else if(allVote== "banana"){
+        banana++;
+    }
+}
+if(mango>banana){
+    return "Mango";
+}
+else if(banana>mango){
+    return "Banana";
+}
+else{
+    return 'Draw'
+}
+}
    
-   let result = avrgVote( ['banana', 'mango','mango']);
-   console.log(result);
+let result = avrgVote(['banana', 'mango','mango']);
+console.log(result);
    
    
