4eff24e17fb75f0ed3ddb1aa035a21a7df2854dc
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-02-21 16:57:43 +0600

    array commit

---
+++ module-24.5/array.js
@@ -0,0 +1,13 @@
+const product = [
+    {name: 'car', price: 200000,},
+    {name: 'honda', price : 10000}
+]
+console.log(product[0]);
+console.log(product[1]);
+
+
+const Product = {
+    '0': 14,
+    '1': 39,
+    '2':23
+}
