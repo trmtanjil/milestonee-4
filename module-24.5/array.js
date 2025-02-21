bd666b60a3e3aeacd881c76e08ec3041a5400f1d
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-02-21 18:40:15 +0600

    ob conver array  commit

--- module-24.5/array.js
+++ module-24.5/array.js
@@ -2,12 +2,19 @@ const product = [
     {name: 'car', price: 200000,},
     {name: 'honda', price : 10000}
 ]
-console.log(product[0]);
-console.log(product[1]);
+// console.log(product[0]);
+// console.log(product[1]);
 
 
 const Product = {
     '0': 14,
     '1': 39,
     '2':23
+};
+let sum = 0;
+let values =Object.values(Product)
+for(let i=0; i<values.length; i++){
+    let valus = values[i];
+    sum = sum+ valus;
 }
+console.log(sum);
